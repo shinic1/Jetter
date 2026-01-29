d43ebc9d9f2628aa82ee8ac77504feafc1c14eb5
shinic1 <bourn31@uwgb.edu>
2026-01-29 17:32:02 -0600

    Initial Commit

---
+++ Jetter/JetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetterApp.swift
+//  Jetter
+//
+//  Created by Nico Bourel on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct JetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
